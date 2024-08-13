731aff5268aaa7a159a5558ba1bebd5171ec30b6
shanky2010 <151531217+shanky2010@users.noreply.github.com>
2024-08-13 19:38:27 +0530

    Add files via upload

---
+++ week1q2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class week1q2{
+	//question 2
+	public static void main(String args[]){		
+        	Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		double d = n/100.0;
+		System.out.format("$%.2f",d);
+	}
+}
