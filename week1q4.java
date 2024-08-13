9d1d9b8674cdb94db8480e8471e399a490aa33d2
shanky2010 <151531217+shanky2010@users.noreply.github.com>
2024-08-13 19:39:42 +0530

    Add files via upload

---
+++ week1q4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class week1q4{
+	//question 4
+	public static void main(String args[]){		
+        	Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		double d = sc.nextDouble();
+		System.out.format("%.2f",((n*d)/100.00)+n);
+	}
+}
